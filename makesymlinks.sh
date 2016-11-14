edef982eebaa33c9b088f37d539e47c273204272
Brad Matola <brmatola@gmail.com>
2016-11-14 15:56:52 -0500

    cleaned up some files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,15 +9,9 @@
 
 dir=~/dotfiles			# dotfiles directory
 olddir=~/dotfiles_old		# old dotfiles backup directory
-files="vim vimrc zshrc zshenv"
+files="zshrc zshenv tmux.conf"
 
 ##########
-
-# create dotfiles_old in homemdir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
@@ -26,23 +20,15 @@ echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then symlink
 for file in $files; do
-	echo "Moving any existing dotfiles from ~ to $olddir"
-	mv ~/.$file ~/dotfiles_old/
 	echo "Creating symlink to $file in home directory."
 	ln -s $dir/$file ~/.$file
 done
 
 # setup nvim in ~/.config
+echo "Creating nvim configuration at ~/.config/nvim"
 rm -rf ~/.config/nvim/
 mkdir ~/.config/nvim/
 ln -s $dir/nvim/autoload ~/.config/nvim/autoload
 ln -s $dir/nvim/config ~/.config/nvim/config
 ln -s $dir/nvim/init.vim ~/.config/nvim/init.vim
 ln -s $dir/nvim/syntax ~/.config/nvim/syntax
-
-unamestr='uname'
-if [[ "$unamestr" == 'Darwin' ]]; then
-    ~/dotfiles/vim/bundle/YouCompleteMe/install.py --clang-completer
-else echo 'You need to finish install of youcompleteme manually'
-fi
-
--- nvim/init.vim
+++ nvim/init.vim
@@ -1,7 +1,7 @@
 " My personal NeoVim configuration
 " Brad Matola brmatola@gmail.com
 
-source ./config/init.vim
-source ./config/general.vim
-source ./config/plugins.vim
-source ./config/keys.vim
+source ~/.config/nvim/config/init.vim
+source ~/.config/nvim/config/general.vim
+source ~/.config/nvim/config/plugins.vim
+source ~/.config/nvim/config/keys.vim
